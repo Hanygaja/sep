962e9b146f429c2c21f957dc5696e3ef95901f07
Hanygaja <53864579+Hanygaja@users.noreply.github.com>
2020-11-09 15:23:17 +0300

    changes

---
+++ process.php
@@ -0,0 +1,36 @@
+<?php
+if(filter_var($_POST['email'], FILTER_VALIDATE_EMAIL))
+{
+$to      = 'hany@securityside.com';
+$from    = 'info@septimiussecurity.com';
+$email   = $_POST['email'];
+$subject = 'Received New Email';
+$message = "Name: ".$_POST['name']."\nPhone: ".$_POST['phone']."\nEmail: ".$_POST['email']."\nSubject: ".$_POST['subject']."\nMessage: ".$_POST['message']."";
+$headers = 'From: '.$from."\r\n" .
+        'Reply-To: '.$email."\r\n" .
+        'X-Mailer: PHP/' . phpversion();
+
+if(mail($to, $subject, $message, $headers))
+{
+	echo'
+	<script>
+	swal("Thank you", "Message has been sent!", "success");
+	</script>
+	';
+	echo'<script>$("#reset")[0].reset()</script>';
+}else
+{
+	echo'
+	<script>
+	swal("Error", "Email not send!", "info");
+	</script>
+	';
+}
+}else{
+	echo'
+	<script>
+	swal("Pay attention", "Enter Valid email!", "info");
+	</script>
+	';
+}
+?>
